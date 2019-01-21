008d8e692ed20b661b79a40d924d9f193ccecd50
vmigdal <volodymyr.migdal@gmail.com>
2019-01-21 11:54:15 +0200

    upgrade

--- out.debug/dwtools/amid/starter/StarterMaker.s
+++ out.debug/dwtools/amid/starter/StarterMaker.s
@@ -3,7 +3,8 @@
 'use strict';
 
 /**
-  @module Tools/mid/Starter - Collection of tools to generate background service to start and pack application. Starter consists of 3 pieces, maker - generating code for run-time, client run-time piece, and server run-time piece. Use the module to keep files structure of the application and make code aware wherein the file system is it executed.
+ * Collection of tools to generate background service to start and pack application. Starter consists of 3 pieces, maker - generating code for run-time, client run-time piece, and server run-time piece. Use the module to keep files structure of the application and make code aware wherein the file system is it executed.
+  @module Tools/mid/Starter
 */
 
 /**
