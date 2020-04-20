c6535b03f85a67e68868153907b3eb94db1ab15f
Wandalen <wandalen.me@gmail.com>
2020-04-20 07:03:38 +0300

    version 0.4.87

---
+++ proto/dwtools/atop/starter.test/_asset/startOptionWithModule/File1.js
@@ -0,0 +1,4 @@
+
+var _ = require( '../../../../../dwtools/Tools.s' );
+_.include( 'wCopyable' );
+console.log( 'File1.js' );
---
+++ proto/dwtools/atop/starter.test/_asset/startWithNpmPackage/File1.js
@@ -0,0 +1,5 @@
+
+debugger;
+var file = require( 'wmodulefortesting1a' );
+debugger;
+console.log( 'File1.js', typeof file, file.squareOfSum( 3 ) );
