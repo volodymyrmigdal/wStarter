94e79460933521265d4f4426c12f3c496af7a2fb
wandalen <wandalen.me@gmail.com>
2020-05-06 12:31:04 +0300

    add test routines

---
+++ proto/dwtools/atop/starter.test/_asset/depGlobAnyAny/in/dir/Dep2.s
@@ -0,0 +1,23 @@
+console.log( 'Dep2.s:begin' );
+
+console.log( `` );
+console.log( `Dep2.s` );
+if( typeof _filePath_ !== 'undefined' )
+console.log( `_filePath_ : ${_filePath_}` );
+if( typeof _dirPath_ !== 'undefined' )
+console.log( `_dirPath_ : ${_dirPath_}` );
+console.log( `__filename : ${__filename}` );
+console.log( `__dirname : ${__dirname}` );
+console.log( `module : ${typeof module}` );
+console.log( `module.parent : ${typeof module.parent}` );
+console.log( `exports : ${typeof exports}` );
+console.log( `require : ${typeof require}` );
+if( typeof include !== 'undefined' )
+console.log( `include : ${typeof include}` );
+if( typeof _starter_ !== 'undefined' )
+console.log( `_starter_.interpreter : ${_starter_.interpreter}` );
+console.log( `` );
+
+module.exports = 'Dep2';
+
+console.log( 'Dep2.s:end' );
---
+++ proto/dwtools/atop/starter.test/_asset/depGlobAnyExt/in/Index.js
@@ -0,0 +1,30 @@
+console.log( 'Index.js:begin' );
+
+debugger;
+var dir = require( './dir/*.(js|s)' );
+
+console.log( `` );
+console.log( `dir.length : ${dir.length}` );
+console.log( `dir[ 0 ] : ${dir[ 0 ]}` );
+console.log( `dir[ 1 ] : ${dir[ 1 ]}` );
+debugger;
+
+console.log( `` );
+console.log( `Index.js` );
+if( typeof _filePath_ !== 'undefined' )
+console.log( `_filePath_ : ${_filePath_}` );
+if( typeof _dirPath_ !== 'undefined' )
+console.log( `_dirPath_ : ${_dirPath_}` );
+console.log( `__filename : ${__filename}` );
+console.log( `__dirname : ${__dirname}` );
+console.log( `module : ${typeof module}` );
+console.log( `module.parent : ${typeof module.parent}` );
+console.log( `exports : ${typeof exports}` );
+console.log( `require : ${typeof require}` );
+if( typeof include !== 'undefined' )
+console.log( `include : ${typeof include}` );
+if( typeof _starter_ !== 'undefined' )
+console.log( `_starter_.interpreter : ${_starter_.interpreter}` );
+console.log( `` );
+
+console.log( 'Index.js:end' );
