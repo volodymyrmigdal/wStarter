853518e96f9af00e20dfb0ab20e14d3cd6939357
wandalen <wandalen.me@gmail.com>
2020-09-28 09:27:27 +0300

    refactoring

---
+++ proto/wtools/atop/starter.test/_asset/startChangeModuleCache/Main.js
@@ -0,0 +1,20 @@
+( function _Main_js_() {
+
+'use strict';
+
+require( './F1.js' );
+
+debugger;
+let Module = require( 'module' );
+debugger;
+let cache = Module._cache;
+Module._cache = Object.create( null );
+
+require( './F1.js' );
+require( './F2.js' );
+
+Module._cache = cache;
+
+require( './F2.js' );
+
+})();
